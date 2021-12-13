8c3b94334a97362800ff8f4e2b04560adf3bc9c1
Ghiftee <gigibetine@gmail.com>
2021-12-13 11:18:09 +0100

    CFix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  root to: 'greetings#index'
-  
-  resources :greetings
+Rails.application.routes.draw do 
+  resources :greetings, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
