0710e5e7b256a247a8dbe61b147eaac9523295de
Ghiftee <gigibetine@gmail.com>
2021-12-13 11:17:18 +0100

    Fix rendering errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,6 @@
 class GreetingsController < ApplicationController
-  # GET /greetings
   def index
     greeting = Greeting.order(Arel.sql('RANDOM()')).first
-    render json: { message: greeting.message }
+    render json: { message: greeting&.message }
   end
 end
